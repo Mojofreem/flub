bc531fc016cc1872ab0156b4ccb7bdd933f808d7
Liam Chasteen <liam.chasteen@gmail.com>
2015-11-23 09:33:34 -0800

    Added cleanup script for CMake generated files.

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# TODO: Check for Makefile, and if present, run "make clean"
+rm -f CMakeCache.txt
+rm -rf CMakeFiles
+rm -f Makefile
+rm -f cmake_install.cmake
+rm -f flub/flubconfig.h
+rm -rf resources
+rm -f stbfont
+rm -f themer
