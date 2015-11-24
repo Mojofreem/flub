6d50e90f5ba4fd6af52e16ffb5623e9073225a3a
Liam Chasteen <liam.chasteen@gmail.com>
2015-11-24 10:05:39 -0800

    Added small dev cleanup script.

--- 3rdparty/physfs-2.0.3/clean.sh
+++ 3rdparty/physfs-2.0.3/clean.sh
@@ -1,6 +1,85 @@
 #!/bin/bash
-rm -f CMakeCache.txt
-rm -rf CMakeFiles
-rm -f libphysfs.dylib
-rm -f libphysfs.2.0.3.dylib
-rm -f test_physfs
+# TODO: Obtain working path from CMake, instead of assuming an in
+#       tree build.
+
+GENERATED_BINS=("libphysfs-flub.a")
+CMAKE_REVERTS=("Doxyfile")
+CMAKE_ARTIFACTS=("CMakeCache.txt" "Makefile" "cmake_install.cmake")
+CMAKE_WORKDIRS=("CMakeFiles")
+
+STASH_EXT=".stash"
+
+function stash_bins {
+	for file in "${GENERATED_BINS[@]}"; do
+		if [ -f "${file}" ]; then
+			cp -f ${file} ${file}${STASH_EXT}
+		fi
+	done	
+}
+
+function unstash_bins {
+	for file in "${GENERATED_BINS[@]}"; do
+		if [ ! -f "${file}${STASH_EXT}" ]; then
+			continue
+		fi
+		cp -f ${file}${STASH_EXT} ${file}
+		rm -f ${file}${STASH_EXT}
+	done	
+}
+
+function clear_cmake_artifacts {
+	for file in "${CMAKE_ARTIFACTS[@]}"; do
+		rm -f ${file}
+	done
+	for path in "${CMAKE_WORKDIRS[@]}"; do
+		rm -rf ${path}
+	done
+	for file in "${CMAKE_REVERTS[@]}"; do
+		git checkout -- ${file}
+	done
+}
+
+function clear_bins {
+	for file in "${GENERATED_BINS[@]}"; do
+		if [ -f "${file}" ]; then
+			rm -f ${file}
+		fi
+	done	
+}
+
+if [ "$1" == "help" ]; then
+	echo "clean.sh <option>"
+	echo
+	echo "    <none> - remove cmake artifacts, build artifacts, and generated"
+	echo "             binaries."
+	echo "    build - remove the build artifacts and generated binaries"
+	echo "    keepbin - remove only the cmake and build artifacts"
+	echo "    help - show this message"
+	return
+elif [ "$1" == "stash" ]; then
+	stash_bins
+	echo "-= Stashed =-"
+elif [ "$1" == "unstash" ]; then
+	unstash_bins
+	echo "-= Unstashed =-"
+elif [ "$1" == "build" ]; then
+	if [ -f "Makefile" ]; then
+		make clean
+	fi
+	clear_bins
+elif [ "$1" == "keepbin" ]; then
+	if [ -f "Makefile" ]; then
+		stash_bins
+		make clean
+		unstash_bins
+		clear_cmake_artifacts
+	fi
+elif [ -z "$1" ]; then
+	if [ -f "Makefile" ]; then
+		make clean
+	fi
+	clear_bins
+	clear_cmake_artifacts
+else
+	echo "ERROR: Unknown command \"${1}\""
+fi
--- clean.sh
+++ clean.sh
@@ -1,10 +1,85 @@
 #!/bin/bash
-# TODO: Check for Makefile, and if present, run "make clean"
-rm -f CMakeCache.txt
-rm -rf CMakeFiles
-rm -f Makefile
-rm -f cmake_install.cmake
-rm -f flub/flubconfig.h
-rm -rf resources
-rm -f stbfont
-rm -f themer
+# TODO: Obtain working path from CMake, instead of assuming an in
+#       tree build.
+
+GENERATED_BINS=("flubtest" "themer" "stbfont")
+CMAKE_REVERTS=()
+CMAKE_ARTIFACTS=("CMakeCache.txt" "Makefile" "cmake_install.cmake" "flub/flubconfig.h")
+CMAKE_WORKDIRS=("CMakeFiles" "resources")
+
+STASH_EXT=".stash"
+
+function stash_bins {
+	for file in "${GENERATED_BINS[@]}"; do
+		if [ -f "${file}" ]; then
+			cp -f ${file} ${file}${STASH_EXT}
+		fi
+	done	
+}
+
+function unstash_bins {
+	for file in "${GENERATED_BINS[@]}"; do
+		if [ ! -f "${file}${STASH_EXT}" ]; then
+			continue
+		fi
+		cp -f ${file}${STASH_EXT} ${file}
+		rm -f ${file}${STASH_EXT}
+	done	
+}
+
+function clear_cmake_artifacts {
+	for file in "${CMAKE_ARTIFACTS[@]}"; do
+		rm -f ${file}
+	done
+	for path in "${CMAKE_WORKDIRS[@]}"; do
+		rm -rf ${path}
+	done
+	for file in "${CMAKE_REVERTS[@]}"; do
+		git checkout -- ${file}
+	done
+}
+
+function clear_bins {
+	for file in "${GENERATED_BINS[@]}"; do
+		if [ -f "${file}" ]; then
+			rm -f ${file}
+		fi
+	done	
+}
+
+if [ "$1" == "help" ]; then
+	echo "clean.sh <option>"
+	echo
+	echo "    <none> - remove cmake artifacts, build artifacts, and generated"
+	echo "             binaries."
+	echo "    build - remove the build artifacts and generated binaries"
+	echo "    keepbin - remove only the cmake and build artifacts"
+	echo "    help - show this message"
+	return
+elif [ "$1" == "stash" ]; then
+	stash_bins
+	echo "-= Stashed =-"
+elif [ "$1" == "unstash" ]; then
+	unstash_bins
+	echo "-= Unstashed =-"
+elif [ "$1" == "build" ]; then
+	if [ -f "Makefile" ]; then
+		make clean
+	fi
+	clear_bins
+elif [ "$1" == "keepbin" ]; then
+	if [ -f "Makefile" ]; then
+		stash_bins
+		make clean
+		unstash_bins
+		clear_cmake_artifacts
+	fi
+elif [ -z "$1" ]; then
+	if [ -f "Makefile" ]; then
+		make clean
+	fi
+	clear_bins
+	clear_cmake_artifacts
+else
+	echo "ERROR: Unknown command \"${1}\""
+fi
